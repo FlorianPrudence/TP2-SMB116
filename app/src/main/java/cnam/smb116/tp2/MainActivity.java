90a82b3ce49988daec5cb3d4470b8fb32664f378
Florian PRUDENCE <florian.prudence@altazion.com>
2023-02-19 15:26:55 +0100

    Fin du TP

--- app/src/main/java/cnam/smb116/tp2/MainActivity.java
+++ app/src/main/java/cnam/smb116/tp2/MainActivity.java
@@ -1,12 +1,20 @@
 package cnam.smb116.tp2;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,4 +37,49 @@ public class MainActivity extends AppCompatActivity {
         i.setData(Uri.parse(url_input.getText().toString())) ;
         startActivity(i);
     }
+
+
+    public void doLogin(View view){
+        Intent intent = new Intent();
+        // Solution pour démarrer le composant de login avec Intent explicite
+        // intent.setComponent(new ComponentName("cnam.smb116.tp2_login","cnam.smb116.tp2_login.MainActivity"));
+
+        // Solution pour démarrer le composant de login avec Intent implicite
+        intent.setAction("TP2_SMB116_LOGIN");
+
+        loginResultLauncher.launch(intent);
+    }
+
+    public void manageOkResult(Intent intentResult) {
+        String username = intentResult.getStringExtra("username");
+        String password = intentResult.getStringExtra("password");
+
+        Context context = getApplicationContext();
+        CharSequence text = "Username : " + username + ", Password : " + password;
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void manageCanceledResult() {
+        Context context = getApplicationContext();
+        CharSequence text = "Login annulé par l'utilisateur";
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    ActivityResultLauncher<Intent> loginResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    manageOkResult(data);
+                }
+                if (result.getResultCode() == Activity.RESULT_CANCELED) {
+                    manageCanceledResult();
+                }
+            });
 }
